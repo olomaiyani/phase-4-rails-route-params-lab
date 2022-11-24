8c547204e10df5681775fe6356284ed83d096bef
Nana <nanaolomaiyani@gmail.com>
2022-11-24 12:24:57 +0300

    commit tree 8781f799d4dc458187cb2cf4487f81717c3f076e

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,7 +2,14 @@ class StudentsController < ApplicationController
 
   def index
     students = Student.all
+    if params[:name] != nil
+      students = students.filter{|student| [student.first_name.downcase, student.last_name.downcase].include?(params[:name].downcase)}
+    end
     render json: students
   end
 
+  def show
+    student = Student.find(params[:id])
+    render json: student 
+  end
 end
